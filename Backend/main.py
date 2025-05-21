8f729528c3d75619606d720d4b4e415e2dd63776
laiba qaiser <laibaqaiser89@gmail.com>
2025-05-21 18:18:14 +0400

    .

--- Backend/main.py
+++ Backend/main.py
@@ -11,3 +11,7 @@ class MessagePayload(BaseModel):
 @app.post("/chat")
 async def receive_message(payload: MessagePayload):
     return {"reply": f"You said: {payload.message}"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Railway will set PORT
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
