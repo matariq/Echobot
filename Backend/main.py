daa3f2dda414c61566bc5e48beb98b9e0dd7bc50
laiba qaiser <laibaqaiser89@gmail.com>
2025-05-21 18:58:09 +0400

    .

--- Backend/main.py
+++ Backend/main.py
@@ -2,16 +2,15 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://echobot-45w9.vercel.app"],  
+    allow_origins=["https://echobot-45w9.vercel.app"," http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-app = FastAPI()
 
 # Define the expected payload structure
 class MessagePayload(BaseModel):
